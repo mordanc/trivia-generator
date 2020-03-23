c3f2f92b1351086e7d8e9f101ee3e22577ff6e15
Evan Rose <eroselax4@hotmail.com>
2020-03-22 20:10:33 -0400

    settings page. need to add cookies

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import he from 'he';
-import Switch from "react-switch";
-import Select from 'react-select';
 import Choice from './Choice';
+import Switch from "react-switch";
 
 class Trivia extends React.Component {
     constructor(props) {
@@ -16,7 +15,7 @@ class Trivia extends React.Component {
             answer: '',
             category: '',
             reveal: false,
-            show_choices: false,
+            show_choices: true,
             selected_choice: '',
             next_disabled: true,
             settings_open: false,
@@ -27,75 +26,106 @@ class Trivia extends React.Component {
     }
 
     toggleChoices(checked) {//change of switch for choices
-        this.setState({ show_choices: checked, selected_choice: '', reveal: false});
+        this.setState({ show_choices: checked, selected_choice: '', reveal: false });
     }
 
-    categoryChange(selected_categories) {
-        this.setState({selected_categories});
+    categoryChanged(category) {
+        const i = this.state.selected_categories.indexOf(category);
+        if (i !== -1) {
+            this.state.selected_categories.splice(i, 1);
+        } else {
+            this.state.selected_categories.push(category);
+        }
+        this.forceUpdate();
+    }
+
+    selectAllCategories() {
+        if(this.state.selected_categories.length > 0) {
+            this.setState({selected_categories: []})
+        }
+        else {
+            var temp = [];
+            this.state.categories.forEach((category) => {
+                temp.push(category);
+            });
+            this.setState({selected_categories: temp});
+        }
     }
 
     setSelected(selected_choice) {
-        if(this.state.selected_choice === '' && !this.state.reveal && this.state.show_choices) {
-            this.setState({selected_choice, show_choices: true, reveal: true, next_disabled: false});
+        if (this.state.selected_choice === '' && !this.state.reveal && this.state.show_choices) {
+            this.setState({ selected_choice, show_choices: true, reveal: true, next_disabled: false });
         }
     }
-    
+
     render() {
         if (this.state.loading) {
-            return <div><img src={require('./images/Wedges-3s-200px.svg')} alt='Loading'/></div>
+            return <div><img src={require('./images/Wedges-3s-200px.svg')} alt='Loading' /></div>
         }
 
         return (
             <div className='trivia-holder'>
-                {this.state.settings_open ?
-                    <div style={{padding: '2vh'}}>
-                        <span onClick={() => this.setState({ settings_open: false })}> [Close] </span>
-                        <Select isMulti options={this.state.categories}
-                        onChange={this.categoryChange.bind(this)}
-                        value={this.state.selected_categories}
-                        // onChange={(category)=>this.setState({category})} value={this.state.categories[0]} placeholder="Select an option"
-
-                        />
-                    </div>
-                    :
+                <img onClick={() => this.setState({ settings_open: !this.state.settings_open })} className='menu-button' src={require('./images/menu-white-48dp.svg')} alt='Settings' />
+                {!this.state.settings_open ?
                     <div>
-                        <span onClick={() => this.setState({ settings_open: true })}>...</span>
-                    </div>
-                }
-                <div className='trivia-text'>
-                    <div className="row">
-                        <div className="type-text">{this.state.type === 'boolean' ? 'True or False' : 'Multiple Choice'}</div>
-                        <Switch onChange={this.toggleChoices.bind(this)} checked={this.state.show_choices} />
-                    </div>
-                    <div className='category-text'>{this.state.category} ({this.state.difficulty})</div>
+                        <div className='trivia-text'>
+                            <div className={'difficulty-text ' + this.state.difficulty}>{this.state.difficulty}</div>
+                            <div className="row">
+                                <div className="type-text">{this.state.type === 'boolean' ? 'True or False' : 'Multiple Choice'}</div>
+                            </div>
+                            <div className='category-text'>{this.state.category}</div>
 
-                    <div className='question-text'>{this.state.question}</div>
+                            <div className='question-text'>{this.state.question}</div>
                             <ul className='choices'>
                                 {this.state.choices.map((choice, key) => {
                                     return (
                                         <li key={key}>
-                                            <Choice 
-                                            text={choice} 
-                                            show={this.state.show_choices}
-                                            correct={this.state.answer === choice}
-                                            reveal={this.state.reveal}
-                                            selected={this.state.selected_choice === choice}
-                                            setSelected={(e)=> this.setSelected(e)}
+                                            <Choice
+                                                text={choice}
+                                                show={this.state.show_choices}
+                                                correct={this.state.answer === choice}
+                                                reveal={this.state.reveal}
+                                                selected={this.state.selected_choice === choice}
+                                                setSelected={(e) => this.setSelected(e)}
                                             />
                                         </li>
                                     );
                                 })}
-                            </ul> 
-                </div>
+                            </ul>
+                        </div>
 
-                <div className='button-holder'>
-                    {this.state.reveal === false ?
-                        <button onClick={() => this.setState({ reveal: true, next_disabled: false, show_choices: true })}>Show Answer</button>
-                        :
-                        <button onClick={() => this.setState({ reveal: false, selected_choice: '' })}>Hide Answer</button>
-                    }
-                    <button hidden={this.state.next_disabled} onClick={() => this.fetchQuestion()}>New Question</button>
-                </div>
+                        <div className='button-holder'>
+                            {!this.state.reveal ?
+                                this.state.show_choices ? <span></span> : <button onClick={() => this.setState({ reveal: true, next_disabled: false, show_choices: true })}>Show Answer</button>
+                                :
+                                <button onClick={() => this.setState({ reveal: false, selected_choice: '' })}>Hide Answer</button>
+                            }
+                            <button hidden={this.state.next_disabled} onClick={() => this.fetchQuestion()}>New Question</button>
+                        </div>
+                    </div>
+                    :
+                    <div style={{ padding: '2vh' }}>
+                        <span onClick={() => this.setState({ settings_open: false })}>Back</span> 
+                        <h3>Settings</h3>
+                        <div className='category-item'>
+                            Show choices
+                            <Switch onChange={this.toggleChoices.bind(this)} checked={this.state.show_choices} />
+                        </div>
+                        <h3>Categories</h3>
+                        <button onClick={()=> this.selectAllCategories()}>Toggle All</button>
+                        {this.state.categories.map((category, index) => {
+                            return (
+                                <div key={index} className='category-item' onClick={()=>this.categoryChanged(category)}>
+                                    {category.name}
+                                    <Switch
+                                        onChange={()=>null}
+                                        checked={this.state.selected_categories.indexOf(category) !== -1}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         );
     }
@@ -128,20 +158,19 @@ class Trivia extends React.Component {
         fetch("https://opentdb.com/api_category.php").then(res => res.json())
             .then(
                 (result) => {
-
+                    let trivia_cats = result.trivia_categories;
                     for (var i = result.trivia_categories.length - 1; i > 0; i--) {// convert json keys from api json to what the dropdown uses
-                        
-                        var obj = result.trivia_categories[i];
-                        obj.value = obj.id;
-                        delete obj.id;
 
-                        obj.label = obj.name;
-                        delete obj.name;
+                        var obj = result.trivia_categories[i];
                         result.trivia_categories[i] = obj;
                     }
+                    let temp = [];
+                    trivia_cats.forEach((cat) => {
+                        temp.push(cat);
+                    });
 
                     this.setState({
-                        categories: result.trivia_categories,
+                        categories: temp,
                         selected_categories: result.trivia_categories
                     });
 
@@ -161,11 +190,11 @@ class Trivia extends React.Component {
         this.setState({ loading: true, settings_open: false });
         var url = "https://opentdb.com/api.php?amount=1";
 
-        if(this.state.session_token !== '') {
-            url += "&token="+this.state.session_token;
+        if (this.state.session_token !== '') {
+            url += "&token=" + this.state.session_token;
         }
-        if(this.state.selected_categories.length < this.state.categories.length && this.state.selected_categories.length > 0) {
-            url += "&category="+this.pickCategory();
+        if (this.state.selected_categories.length < this.state.categories.length && this.state.selected_categories.length > 0) {
+            url += "&category=" + this.pickCategory();
         }
 
 
@@ -178,7 +207,6 @@ class Trivia extends React.Component {
                         reveal: false,
                         loading: false,
                         next_disabled: true,
-                        show_choices: false,
                         selected_choice: '',
                         type: result.results[0].type,
                         difficulty: result.results[0].difficulty,
@@ -217,7 +245,8 @@ class Trivia extends React.Component {
 
     pickCategory() {
         var index = Math.floor(Math.random() * (this.state.selected_categories.length));
-        return this.state.selected_categories[index].value;
+        console.log("PICK CATEGORY: " + this.state.selected_categories[index].id);
+        return this.state.selected_categories[index].id;
     }
 
     // Scramble the choices so correct position isn't predictable
